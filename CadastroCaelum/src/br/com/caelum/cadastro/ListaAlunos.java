8db69a4392566e6d83f6b384898e8a74a8f1013a
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2013-09-25 00:12:22 -0300

    Implementação de ListaAlunos.java e listagem_alunos.xml

--- CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
@@ -1,8 +1,14 @@
 package br.com.caelum.cadastro;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListaAlunos extends Activity {
 
@@ -10,13 +16,34 @@ public class ListaAlunos extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listagem_alunos);
-	}
+		
+		final String[] nomes = {"Ana", "José", "Felipe"};
+		
+		int layout = android.R.layout.simple_list_item_1;	
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layout, nomes);
+		
+		ListView lista = (ListView) findViewById(R.id.lista);
+		lista.setAdapter(adapter);
+		
+		lista.setOnItemClickListener(new OnItemClickListener() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.lista_alunos, menu);
-		return true;
-	}
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view, int posicao,
+					long id) {
+				
+				Toast.makeText(ListaAlunos.this, "Clique na posição "+ posicao, Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		lista.setOnItemLongClickListener(new OnItemLongClickListener() {
 
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View view,
+					int posicao, long id) {
+				Toast.makeText(ListaAlunos.this, "Clique longo em "+ adapter.getItemAtPosition(posicao), Toast.LENGTH_SHORT).show();
+				return true;
+			}
+		});
+	}
 }
